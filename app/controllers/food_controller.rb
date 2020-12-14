2230e9056ceae4b8bee3133d54cf26dc6786404b
Brett Turing <bjsherman80@gmail.com>
2020-12-14 09:47:19 -0700

    building api call in index but getting nil response

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -1,3 +1,14 @@
-class FoodController < ApplicationCointroller
+class FoodController < ApplicationController
 
+  def index
+    @food = params[:q]
+
+    conn = Faraday.new(url: "https://api.nal.usda.gov/")
+
+    response = conn.get("fdc/v1/search") do |f|
+      f.headers["X-Api-Key"] = ENV['FOOD_API_KEY']
+      f.params['query'] = @food               
+    binding.pry
+    end
+  end
 end
